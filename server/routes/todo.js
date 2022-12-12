72d2d629709c10f814fcd1e65a9498315915fbae
2hyeseung <leehs226@naver.com>
2022-12-12 12:25:48 +0900

    feat: Todo 수정, 삭제 구현

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -27,4 +27,42 @@ router.post("/todo", async (req, res) => {
     }
 });
 
+// PATCH localhost:PORT/todo/:todoId - edit a specific todo (UPDATE)
+router.patch("/todo/:todoId", async (req, res) => {
+    // 수정 성공시 [1], 수정 실패시 [0]
+    try {
+        let [isUpdated] = await Todo.update(
+            {
+                title: req.body.title,
+                done: req.body.done,
+            },
+            {
+                where: { id: req.params.todoId },
+            }
+        );
+        if (!isUpdated) {
+            return res.send(false);
+        }
+        res.send(true);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+router.delete("/todo/:todoId", async (req, res) => {
+    try {
+        let isDeleted = await Todo.destroy({
+            where: {
+                id: req.params.todoId,
+            },
+        });
+        if (!isDeleted) {
+            return res.send(false);
+        }
+        res.send(true);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
 module.exports = router;
