8855be99064b8ab206a6443530a6efd1e1261199
2hyeseung <leehs226@naver.com>
2022-12-12 15:16:22 +0900

    feat: Todo 리스트 보이기

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import Todo from "./components/Todo";
+
 const App = () => {
-    return <div className="App">My Todo App</div>;
+    const [todoItems, setTodoItems] = useState([
+        { id: 1, title: "MyTodo1", done: false },
+        { id: 2, title: "MyTodo2", done: false },
+        { id: 3, title: "MyTodo3", done: true },
+    ]);
+    return (
+        <div className="App">
+            {todoItems.map((item) => {
+                // console.log(item); // {id: 1, title: 'My Todo1', done: false}
+                return <Todo key={item.id} item={item} />;
+            })}
+        </div>
+    );
 };
 
 export default App;
