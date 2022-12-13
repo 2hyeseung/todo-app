85fc0774d31394a4053250d2433510b4b10016a9
2hyeseung <leehs226@naver.com>
2022-12-13 15:00:23 +0900

    feat: Todo 수정 기능 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import { useState, useRef } from "react";
-import Todo from "./components/Todo";
 import AddTodo from "./components/AddTodo";
+import Todo from "./components/Todo";
 
 const App = () => {
     const [todoItems, setTodoItems] = useState([
-        { id: 1, title: "MyTodo1", done: false },
-        { id: 2, title: "MyTodo2", done: false },
-        { id: 3, title: "MyTodo3", done: true },
+        {
+            id: 1,
+            title: "My Todo1",
+            done: false,
+        },
+        {
+            id: 2,
+            title: "My Todo2",
+            done: false,
+        },
+        {
+            id: 3,
+            title: "My Todo3",
+            done: true,
+        },
     ]);
     const todoId = useRef(4);
 
@@ -14,18 +26,29 @@ const App = () => {
     // 상위 컴포넌트(App)은 AddTodo 컴포넌트 접근 가능
     // => App 컴포넌트에 addItem() 함수를 정의하고, 해당 함수를 AddTodo props로 넘겨야 함
     const addItem = (newItem) => {
-        // newItem - {id: ?, title: ?, done: false}
+        // newItem - {id: xx, title: xx, done: false}
         newItem.id = todoId.current++; // key를 위한 id 설정
         newItem.done = false; // done 초기화
         // 기존 todoItems를 유지하고, 새로운 newItem을 추가
         setTodoItems([...todoItems, newItem]); // setTodoItems(todoItems.concat(newItem))
     };
 
+    // 전체 Todo 리스트(todoItems)는 App 컴포넌트에서 관리하고 있으므로
+    // deleteItem() 함수는 App 컴포넌트에 작성해야 함
+    const deleteItem = (targetItem) => {
+        let newTodoItems = todoItems.filter(
+            (item) => item.id !== targetItem.id
+        );
+        setTodoItems(newTodoItems);
+    };
+
     return (
         <div className="App">
             <AddTodo addItem={addItem} />
             {todoItems.map((item) => {
-                return <Todo key={item.id} item={item} />;
+                return (
+                    <Todo key={item.id} item={item} deleteItem={deleteItem} />
+                );
             })}
         </div>
     );
--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,5 +1,46 @@
-const Todo = ({ item }) => {
+import { useState } from "react";
+
+const Todo = ({ item, deleteItem }) => {
+    // console.log(item); // { id: 1, title: 'todo1', done: false, }
     const { id, title, done } = item;
+    const [todoItem, setTodoItem] = useState(item);
+    const [readOnly, setReadOnly] = useState(true);
+
+    const onDeleteBtnClick = () => {
+        deleteItem(todoItem);
+    };
+
+    // title input 커서가 깜빡인다고 수정이 가능한 것은 아님
+    // 사용자가 키보드 입력할 때마다 todoItem의 title을 새 값으로 변경
+    const editEventHandler = (e) => {
+        // rest: id, done 정보
+        const { title, ...rest } = todoItem; // { id: 1, title: 'todo1', done: false, }
+
+        setTodoItem({
+            title: e.target.value,
+            ...rest,
+        });
+    };
+
+    // title input 클릭시 (title를 수정하겠다!!) : readOnly state를 false로 변경
+    const offReadOnlyMode = () => {
+        setReadOnly(false);
+    };
+
+    // title input에서 enter 키 입력시 (title 수정을 완료했다!!): readOnly state를 true로 변경
+    const enterKeyEventHandler = (e) => {
+        if (e.key === "Enter") {
+            setReadOnly(true);
+        }
+    };
+
+    const checkboxEventHandler = (e) => {
+        const { done, ...rest } = todoItem;
+        setTodoItem({
+            done: !done,
+            ...rest,
+        });
+    };
 
     return (
         <div className="Todo">
@@ -9,8 +50,22 @@ const Todo = ({ item }) => {
                 name={`todo${id}`}
                 value={`todo${id}`}
                 defaultChecked={done}
+                onChange={checkboxEventHandler}
             />
-            <label htmlFor={`todo${id}`}>{title}</label>
+            {readOnly ? (
+                <label htmlFor={`todo${id}`} onClick={offReadOnlyMode}>
+                    {todoItem.title}
+                </label>
+            ) : (
+                <input
+                    type="text"
+                    value={todoItem.title}
+                    onChange={editEventHandler}
+                    onKeyPress={enterKeyEventHandler}
+                    readOnly={readOnly}
+                />
+            )}
+            <button onClick={onDeleteBtnClick}>DELETE</button>
         </div>
     );
 };
